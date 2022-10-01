553e3448cfcfb0661d4d97e955d8c545db97ab89
Vladislav Sokolov <vladislav.k.sokolov@gmail.com>
2022-10-01 17:46:42 +0300

    Gitignore +UniRx

---
+++ Assets/Utils/ListExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ListExtensions
+{
+    public static T GetRandom<T>(this List<T> list)
+    {
+        if (list.Count == 0) return default;
+
+        int rand = UnityEngine.Random.Range(0, list.Count);
+        return list[rand];
+    }
+    public static T ExtractRandom<T>(this List<T> list)
+    {
+        if (list.Count == 0) return default;
+        
+        int rand = UnityEngine.Random.Range(0, list.Count);
+        var result = list[rand];
+        list.RemoveAt(rand);
+        return result;
+    }
+    
+    public static bool TryExtractRandom<T>(this List<T> list, out T item)
+    {
+        item = default;
+        if (list.Count == 0) return false;
+        
+        int rand = UnityEngine.Random.Range(0, list.Count);
+        item = list[rand];
+        list.RemoveAt(rand);
+        return true;
+    }
+}
